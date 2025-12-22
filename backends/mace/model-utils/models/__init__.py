8d520a8eea51300a538752005457c843448689bb
kjt222 <2609641508@qq.com>
2025-12-22 10:51:25 +0800

    Move MACE backend into backends/mace

--- backends/mace/model-utils/models/__init__.py
+++ backends/mace/model-utils/models/__init__.py
@@ -10,7 +10,6 @@ from .registry import (
     build_model_from_json,
 )
 from .model_mace import instantiate_model, default_architecture
-from . import model_equiformerv2  # noqa: F401
 
 __all__ = [
     "ModelBuilder",
